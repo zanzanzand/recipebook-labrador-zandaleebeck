01cf5e65ed4083e8f2a803e9bdef177d0918fdea
Zandalee <zandalee.labrador@student.ateneo.edu>
2025-02-13 19:58:04 +0800

    fixed imports

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import recipes, recipe
+from .views import recipes, recipe1, recipe2
 
 urlpatterns = [
     path('recipes/list', recipes, name='recipes'),
@@ -8,4 +8,4 @@ urlpatterns = [
     path('recipe/2', recipe2, name='recipe-2')
 ]
 
-app_name='ledger'
+app_name = 'ledger'
