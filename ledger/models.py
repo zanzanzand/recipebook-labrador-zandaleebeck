9b1acf3d171c9c1d28d7f7ae4df8734d732673ee
Zandalee <zandalee.labrador@student.ateneo.edu>
2025-02-27 15:42:41 +0800

    added required models

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=120)
+   
+    def __str__(self):
+        return self.name
+   
+    def get_absolute_url(self):
+        return reverse('ledger:recipe-detail', args=str(self.pk))
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=120)
+   
+    def __str__(self):
+        return self.name
+   
+    def get_absoulute_url(self):
+        return reverse('ledger:ingredient-detail', args=str(self.pk))
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+   
+    def __str__(self):
+        return f'{self.quantity} - {self.ingredient.name}'
