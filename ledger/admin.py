552cb4b1052ac22599ae8ed628e14f626da468c6
Zandalee <zandalee.labrador@student.ateneo.edu>
2025-02-27 19:17:26 +0800

    fixed for pep8 convention

--- ledger/admin.py
+++ ledger/admin.py
@@ -5,8 +5,10 @@ from .models import Recipe, RecipeIngredient
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [RecipeIngredientInline]
-    
+
+
 admin.site.register(Recipe, RecipeAdmin)
