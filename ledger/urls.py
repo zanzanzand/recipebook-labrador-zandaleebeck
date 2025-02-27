0a4549e0875bf600f51eafdb76059d48189afe63
Zandalee <zandalee.labrador@student.ateneo.edu>
2025-02-27 16:34:51 +0800

    manual url to dynamic

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-
-from .views import recipes, recipe1, recipe2
+from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipes/list', recipes, name='recipes'),
-    path('recipe/1', recipe1, name='recipe-1'),
-    path('recipe/2', recipe2, name='recipe-2')
+    path('recipes/list', RecipeListView.as_view(), name='recipes'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail'),
 ]
 
 app_name = 'ledger'
