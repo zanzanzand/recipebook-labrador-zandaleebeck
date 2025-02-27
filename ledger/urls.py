bfd49ff3e225e92141b35be3557d2ef3b6d07d3c
Zandalee <zandalee.labrador@student.ateneo.edu>
2025-02-27 18:48:12 +0800

    fixed name

--- ledger/urls.py
+++ ledger/urls.py
@@ -3,7 +3,7 @@ from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipes'),
-    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
 
 app_name = 'ledger'
