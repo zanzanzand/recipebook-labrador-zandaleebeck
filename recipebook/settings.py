530c1435a7f412a6757bd083561151a2d71265fb
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-03-10 22:22:06 +0800

    added login and logout redirect

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = "../../recipes/list"
+LOGOUT_REDIRECT_URL = "../../accounts/login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
