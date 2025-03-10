dfa688dfaf9249eaf754781d17a0974cb587b7fa
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-03-10 15:42:57 +0800

    removed profile

--- ledger/models.py
+++ ledger/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator
 
 
 class Recipe(models.Model):
@@ -38,13 +36,4 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f'{self.ingredient.name} - {self.quantity}'
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bio = models.TextField(
-        validators=[
-            MinLengthValidator(256, "Bio is too short. It must be at least 256 characters.")
-        ]
-    )   
+  
