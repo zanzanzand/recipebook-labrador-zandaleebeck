e947112e15264d38f5851c469ced975cb17812c2
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-03-10 15:46:22 +0800

    added accounts app

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(256, "Bio is too short. It must be at least 256 characters.")
+        ]
+    ) 
