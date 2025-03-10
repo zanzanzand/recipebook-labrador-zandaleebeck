bb1c4923665b05c92a4bd1f67eedddc8ff08b883
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-03-10 15:44:46 +0800

    added path to accounts

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('ledger.urls', namespace= 'ledger')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
