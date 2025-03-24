228383bdc66d504e11d6125f6eb1aea3f8112ea9
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-03-24 17:26:25 +0800

    updated to createview

--- ledger/views.py
+++ ledger/views.py
@@ -1,7 +1,35 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Recipe
+from django.view.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    template_name = "recipe_add.html"
+    form_class = RecipeForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
+
+    
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    template_name = "recipe_add_image.html"
+    form_class = RecipeImageForm
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "ledger:recipe", kwargs={"pk": self.object.recipe.pk}
+        )
+
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs["pk"])
+        return super().form_valid(form)
 
 
 class RecipeListView(ListView):
