0b728d2e8383fab9d9e3fbda109100eb7b63c8b5
zanzanzand <zandalee.labrador@student.ateneo.edu>
2025-03-24 17:29:23 +0800

    added create recipe and add image paths

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeImageCreateView
 
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipes'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe/add', RecipeCreateView.as_view(), name="recipe_create"),
+    path(
+        'recipe/<int:pk>/add_image',
+        RecipeImageCreateView.as_view(),
+        name = 'recipe_add_image',
+    ),
 ]
 
 app_name = 'ledger'
